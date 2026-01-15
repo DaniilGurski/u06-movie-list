67c722b81356abdf61c496eda8e1029ec231db0b
Adam <181141344+breezywideman@users.noreply.github.com>
2026-01-15 17:54:56 +0100

    Cleaned up details page and fixed typos

--- client/src/pages/details/index.ts
+++ client/src/pages/details/index.ts
@@ -5,15 +5,14 @@ export default (movieId: number) => {
 
     document.title = "Details";
 
-    document.createElement("div");
-    const browse = document.createDocumentFragment();
+    const details = document.createDocumentFragment();
     const content = document.createDocumentFragment();
 
     const heading = document.createElement("h1");
     heading.textContent = `Details for movie ${movieId}`;
 
     content.append(heading);
-    browse.append(header(), content, footer());
+    details.append(header(), content, footer());
 
-    return browse;
+    return details;
 };
