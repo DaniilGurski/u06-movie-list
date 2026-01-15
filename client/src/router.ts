4f2efc68a0c49e693a99adc90e790d7e07e11309
Adam <181141344+breezywideman@users.noreply.github.com>
2026-01-15 17:59:59 +0100

    Task 4.1 - details page + rotuer logic (#9)
    * Added a simple details page that displays movieId. Added logic in router to route users to the 'right' details page
    
    * Cleaned up details page and fixed typos

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,9 +1,27 @@
 import browse from "./pages/browse";
 import watchlist from "./pages/watchlist";
 import notFound from "./pages/notFound";
+import details from "./pages/details";
 
 const router = (): HTMLElement | DocumentFragment => {
     const path = window.location.pathname;
+
+    // Om path börjar med /details, extrahera id/nummer om det finns
+
+    if (path.startsWith("/details")) {
+        const movieId = Number(path.split("/")[2]);
+
+        // Visa 404-sidan om id/nummer saknas, är noll, negativt eller inte är ett nummer
+
+        if (isNaN(movieId) || movieId <= 0) {
+            return notFound();
+        }
+
+        // Annars visa details sidan med aktuellt id/nummer
+
+        return details(movieId);
+    }
+
     switch (path) {
         case "/":
         case "/browse":
