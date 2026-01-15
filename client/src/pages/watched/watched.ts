897b7e2f01d9b940dfeec936169b6de8559ae8bf
dogge84@hotmail.com <dogge84@hotmail.com>
2026-01-15 15:10:14 +0100

    added watched.ts file

---
+++ client/src/pages/watched/watched.ts
@@ -0,0 +1,28 @@
+import header from "../../components/header";
+import footer from "../../components/footer";
+import SearchField from "../../components/search";
+import MovieList from "../../components/movieList";
+import { getMovies, getSearchInputValue } from "../../lib/store";
+
+export default function browse() {
+    const browse = document.createDocumentFragment();
+    const content = document.createDocumentFragment();
+
+    const heading = document.createElement("h2");
+    heading.setAttribute("id", "browse-heading");
+    const defaultSearchInputValue = getSearchInputValue();
+
+    heading.textContent =
+        defaultSearchInputValue.trim() !== "" ? "Results" : "Popular";
+
+    const searchField = SearchField();
+
+    // Get movies from store and render list
+    const movies = getMovies();
+    const movieList = MovieList(movies);
+
+    content.append(searchField, heading, movieList);
+    browse.append(header(), content, footer());
+
+    return browse;
+}
