bd87c0f13bb01d0a4286c4523145fce6a8717aff
Isak Nywall <nywall@pm.me>
2026-01-22 13:17:18 +0100

    feat: use better types in store and movieApi
    gets them from the already existing types instead of repeating them

--- client/src/pages/details/index.ts
+++ client/src/pages/details/index.ts
@@ -72,8 +72,8 @@ export default async (movie: TMDBMovie) => {
             if (!savedMovie.id) return;
 
             await updateMovieData(savedMovie.id, {
-                personal_rating: personalRating ? Number(personalRating) : null,
-                review: review || null,
+                personal_rating: personalRating ? Number(personalRating) : undefined,
+                review: review || undefined,
             });
 
             statusSpan.textContent = "Changes saved !";
