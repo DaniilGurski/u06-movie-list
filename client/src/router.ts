24f71fca47a8694a20ef7e94ffd300a9e048c598
Isak Nywall <nywall@pm.me>
2026-01-22 11:51:31 +0100

    chore: remove test page

--- client/src/router.ts
+++ client/src/router.ts
@@ -4,7 +4,6 @@ import watchlist from "./pages/watchlist";
 import watched from "./pages/watched";
 import notFound from "./pages/notFound";
 import details from "./pages/details";
-import test from "./pages/test"; // Testsida för movieList.ts
 import { getMovieByIdTMDB } from "./services/tmdbApi";
 import { getUserListCached } from "./lib/store";
 
@@ -63,8 +62,6 @@ const router = async (): Promise<HTMLElement | DocumentFragment> => {
             return watchlist();
         case "/watched":
             return watched();
-        case "/test":
-            return test();
         default:
             return notFound();
     }
