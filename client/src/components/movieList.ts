4a396e76f1867079d502f5669791b2b7ce5d7d6c
ErnestIssa <ernestisa32@gmail.com>
2026-01-26 13:11:41 +0100

    saving my code for personal use

--- client/src/components/movieList.ts
+++ client/src/components/movieList.ts
@@ -1,20 +1,45 @@
-import type { TMDBMovie } from "../types/movie";
+import type { MovieListConfig } from "../types/movie";
 import MovieItem from "./movieItem";
+import { noMoviesInList } from "../components/noMoviesInList";
 
-export default function MovieList(movies: TMDBMovie[]) {
-    const list = document.createElement("ul");
+export default function MovieList(config: MovieListConfig) {
 
-    if (movies.length === 0) {
-        const emptyMessage = document.createElement("li");
-        emptyMessage.textContent = "No movies found";
-        list.appendChild(emptyMessage);
-        return list;
+    const { movies, itemConfig, heading, introduction } = config;
+
+    const section = document.createElement("section");
+    section.classList.add("page", "container");
+
+    if (heading) {
+        const h1 = document.createElement("h1");
+        h1.className = "page__title";
+        h1.textContent = heading;
+        section.appendChild(h1);
     }
 
-    movies.forEach((movie) => {
-        const listItem = MovieItem(movie);
-        list.appendChild(listItem);
-    });
+    if (introduction) {
+        const p = document.createElement("p");
+        p.className = "page__introduction";
+        p.textContent = introduction;
+        section.appendChild(p);
+    }
 
-    return list;
+    const list = document.createElement("ul");
+    list.classList.add("movie-cards");
+
+    if (movies.length === 0) {
+        const emptyState = noMoviesInList("/src/assets/images/big_chungus.webp", "No movies in this list", "Big Chungus says: maybe you hate movies?");
+        section.appendChild(emptyState);
+    } else {
+        const list = document.createElement("ul");
+        list.classList.add("movie-cards");
+
+        movies.forEach((movie) => {
+            const card = MovieItem({ movie, ...itemConfig });
+            list.appendChild(card);
+        });
+
+        section.appendChild(list);
+    }
+
+    return section;
 }
